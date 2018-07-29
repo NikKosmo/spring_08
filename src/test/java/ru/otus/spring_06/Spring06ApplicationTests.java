59ca9714b718d9cb20133571b351de23abf7a616
NikKosmo <nik_kosmo@mail.ru>
2018-07-29 20:17:42 +0300

    Initial commit

---
+++ src/test/java/ru/otus/spring_06/Spring06ApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.otus.spring_06;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring06ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
