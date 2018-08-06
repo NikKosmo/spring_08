6e88d2a389f740d6ffc7e19099b9790bbe88ed40
NikKosmo <nik_kosmo@mail.ru>
2018-08-06 22:07:08 +0300

    Move to Jpa from Jdbc

---
+++ src/main/java/ru/otus/spring_07/repository/BookRepository.java
@@ -0,0 +1,20 @@
+package ru.otus.spring_07.repository;
+
+import ru.otus.spring_07.damain.Book;
+
+import java.util.List;
+
+public interface BookRepository {
+
+    int count();
+
+    void insert(Book book);
+
+    void delete(int id);
+
+    Book getById(int id);
+
+    Book getByName(String name);
+
+    List<Book> getAll();
+}
---
+++ src/main/java/ru/otus/spring_07/repository/BookRepositoryJpa.java
@@ -0,0 +1,55 @@
+package ru.otus.spring_07.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.spring_07.damain.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Transactional
+@Repository
+public class BookRepositoryJpa implements BookRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public int count() {
+        TypedQuery<Long> query = em.createQuery("select count(b) from Book b", Long.class);
+        return query.getSingleResult().intValue();
+    }
+
+    @Override
+    public void insert(Book book) {
+        em.persist(book);
+    }
+
+    @Override
+    public void delete(int id) {
+        Query query = em.createQuery("delete from Book b where b.id = :id");
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+
+    @Override
+    public Book getById(int id) {
+        return em.find(Book.class, id);
+    }
+
+    @Override
+    public Book getByName(String name) {
+        TypedQuery<Book> query = em.createQuery("select b from Book b where b.name = :name", Book.class);
+        query.setParameter("name", name);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public List<Book> getAll() {
+        TypedQuery<Book> query = em.createQuery("select b from Book b", Book.class);
+        return query.getResultList();
+    }
+}
